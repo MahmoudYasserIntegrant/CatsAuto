4807b6e99bfdf21e70e015e50b8f0cc52d82271d
Noha85 <nibrahim@integrant.com>
2022-01-05 19:29:28 +0200

    QC Version

---
+++ src/test/java/pages/CatsMenu.java
@@ -0,0 +1,74 @@
+package pages;
+
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.utility.Locators_Repo;
+
+public class CatsMenu extends Locators_Repo  {
+	
+
+	public CatsMenu(WebDriver browserObject) {
+		
+		this.browserObject= browserObject;
+		Action = new Actions(browserObject);
+	}
+	
+
+	public void NavigateToSoldGroups() {
+		
+		 
+		WebElement element = (new WebDriverWait(browserObject, 50))
+				   .until(ExpectedConditions.elementToBeClickable(By.linkText("Sold Groups")));
+		Action.moveToElement(element).build().perform();	
+		WebElement element1 = browserObject.findElement(SoldGroups);
+		 browserObject.manage().timeouts().implicitlyWait(50,TimeUnit.SECONDS) ;
+		 element1.click();	
+	}
+
+	public void NavigateToEmployerSettingsScreen(String CustomerNum) {
+		
+		WebElement element = browserObject.findElement(GroupNumberTextBox);
+		browserObject.manage().timeouts().implicitlyWait(5000,TimeUnit.SECONDS) ;
+		element.sendKeys(CustomerNum);
+		
+	
+		WebElement element1 = browserObject.findElement(SoldGroupsSearchBtn);
+		browserObject.manage().timeouts().implicitlyWait(5000,TimeUnit.SECONDS) ;
+		element1.click();
+				
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement element2 = browserObject.findElement(SoldGroupsEnrollmentHyberLink);
+		browserObject.manage().timeouts().implicitlyWait(5000,TimeUnit.SECONDS) ;
+		element2.click();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement element3 = browserObject.findElement(EmployerGroupSettingsBtn);
+		browserObject.manage().timeouts().implicitlyWait(5000,TimeUnit.SECONDS) ;
+		element3.click();
+	}
+
+
+}
+
+
