36e5366335d541a769a1628cbd9cace5bfcded6a
NessmaY <nessma.youssef@integrant.com>
2022-06-14 13:59:28 +0200

    Adding SideBarmenu classes to select Agents assignment tab

---
+++ src/test/java/pages/QuotingSideBarMenu.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.utility.QuotingSideBarMenu_Locators;
+
+public class QuotingSideBarMenu extends QuotingSideBarMenu_Locators {
+	
+	public QuotingSideBarMenu (WebDriver browserObject)
+	{
+		this.browserObject= browserObject;
+	}
+
+	public void NavigateToGroup()
+	{
+		WebDriverWait wait = new WebDriverWait(browserObject, 10);
+		browserObject.findElement(GroupNumber_Link).click();
+		
+		wait.until(ExpectedConditions.titleContains("Customer Details"));
+	}
+	
+	public void NavigateToAgentsAssignment()
+	{
+		WebDriverWait wait = new WebDriverWait(browserObject, 10);
+		browserObject.findElement(AgentsAssignment_SideBarItem).click();
+		
+		wait.until(ExpectedConditions.titleContains("Agents Assignment"));
+	}
+}
