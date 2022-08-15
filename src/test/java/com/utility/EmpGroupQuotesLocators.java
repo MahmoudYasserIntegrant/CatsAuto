a499a568d7913e61f6b24fb7303868794296f59d
NIbrahim <NIbrahim@EG-JXSJRG3.STA.com>
2022-08-15 15:10:15 +0200

    add commit

---
+++ src/test/java/com/utility/EmpGroupQuotesLocators.java
@@ -0,0 +1,15 @@
+package com.utility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class EmpGroupQuotesLocators {
+
+	public WebDriver browserObject ;
+	public By quotesLst = By.id("ctl00_DefaultContent_uxCustomerQuotesList");
+	public By GenerateQuoteBtn = By.id("ctl00_DefaultContent_uxPrintCommand");
+	public By DoneBtn = By.id("ctl00_DefaultContent_uxDoneCommand");
+	
+// test //
+	
+
+}
